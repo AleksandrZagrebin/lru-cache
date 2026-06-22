bfd3fb0eb31ceaa83eede53bbd12ae2d0d1b3ff6
Aleksandr Zagrebin <132539805+AleksandrZagrebin@users.noreply.github.com>
2026-06-22 17:17:37 +0400

    Update test_list.c
    Тесты переписаны под полную инкапсуляцию списка

--- test_list.c
+++ test_list.c
@@ -26,7 +26,6 @@
 #include <string.h>
 #include <assert.h>
 
-// Тест 1: Создание и уничтожение списка
 static void test_create() {
     printf("TEST: list_create ... ");
     struct List_t* list = list_create();
@@ -39,7 +38,6 @@ static void test_create() {
     printf("PASSED\n");
 }
 
-// Тест 2: Добавление элементов в голову
 static void test_push_front() {
     printf("TEST: list_push_front ... ");
     struct List_t* list = list_create();
@@ -50,8 +48,8 @@ static void test_push_front() {
     list_push_front(list, 2, b);
     list_push_front(list, 3, c);
     assert(list_size(list) == 3);
-    assert(list_head(list)->key == 3);
-    assert(list_tail(list)->key == 1);
+    assert(list_get_key(list_head(list)) == 3);
+    assert(list_get_key(list_tail(list)) == 1);
     free(a);
     free(b);
     free(c);
@@ -59,7 +57,6 @@ static void test_push_front() {
     printf("PASSED\n");
 }
 
-// Тест 3: Перемещение существующих узлов в голову
 static void test_move_to_front() {
     printf("TEST: list_move_to_front ... ");
     struct List_t* list = list_create();
@@ -71,11 +68,11 @@ static void test_move_to_front() {
     list_push_front(list, 3, c);
     
     list_move_to_front(list, n1);
-    assert(list_head(list)->key == 1);
-    assert(list_tail(list)->key == 2);
+    assert(list_get_key(list_head(list)) == 1);
+    assert(list_get_key(list_tail(list)) == 2);
     list_move_to_front(list, list_tail(list));
-    assert(list_head(list)->key == 2);
-    assert(list_tail(list)->key == 3);
+    assert(list_get_key(list_head(list)) == 2);
+    assert(list_get_key(list_tail(list)) == 3);
     
     free(a);
     free(b);
@@ -84,7 +81,6 @@ static void test_move_to_front() {
     printf("PASSED\n");
 }
 
-// Тест 4: Удаление элементов из хвоста
 static void test_pop_back() {
     printf("TEST: list_pop_back ... ");
     struct List_t* list = list_create();
@@ -98,12 +94,12 @@ static void test_pop_back() {
     int popped = list_pop_back(list);
     assert(popped == 1);
     assert(list_size(list) == 2);
-    assert(list_tail(list)->key == 2);
+    assert(list_get_key(list_tail(list)) == 2);
     popped = list_pop_back(list);
     assert(popped == 1);
     assert(list_size(list) == 1);
-    assert(list_head(list)->key == 3);
-    assert(list_tail(list)->key == 3);
+    assert(list_get_key(list_head(list)) == 3);
+    assert(list_get_key(list_tail(list)) == 3);
     popped = list_pop_back(list);
     assert(popped == 1);
     assert(list_size(list) == 0);
@@ -118,7 +114,6 @@ static void test_pop_back() {
     printf("PASSED\n");
 }
 
-// Тест 5: Удаление произвольных узлов
 static void test_remove_node() {
     printf("TEST: list_remove_node ... ");
     struct List_t* list = list_create();
@@ -127,18 +122,18 @@ static void test_remove_node() {
     int* c = malloc(sizeof(int)); *c = 300;
     struct ListNode_t* n1 = list_push_front(list, 1, a);
     struct ListNode_t* n2 = list_push_front(list, 2, b);
-    struct ListNode_t* n3 = list_push_front(list, 3, c);
+    list_push_front(list, 3, c);
     assert(list_size(list) == 3);
     list_remove_node(list, n2);
     assert(list_size(list) == 2);
-    assert(list_head(list) == n3);
-    assert(list_tail(list) == n1);
-    assert(n3->next == n1);
-    assert(n1->prev == n3);
+    assert(list_get_key(list_head(list)) == 3);
+    assert(list_get_key(list_tail(list)) == 1);
+    assert(list_node_get_next(list_head(list)) == list_tail(list));
+    assert(list_node_get_prev(list_tail(list)) == list_head(list));
     list_remove_node(list, list_head(list));
     assert(list_size(list) == 1);
-    assert(list_head(list) == n1);
-    assert(list_tail(list) == n1);
+    assert(list_get_key(list_head(list)) == 1);
+    assert(list_get_key(list_tail(list)) == 1);
     list_remove_node(list, list_head(list));
     assert(list_size(list) == 0);
     assert(list_head(list) == NULL);
@@ -150,7 +145,6 @@ static void test_remove_node() {
     printf("PASSED\n");
 }
 
-// Тест 6: Очистка списка
 static void test_clear() {
     printf("TEST: list_clear ... ");
     struct List_t* list = list_create();
@@ -173,16 +167,15 @@ static void test_clear() {
     printf("PASSED\n");
 }
 
-// Тест 7: Работа с одним элементом
 static void test_single_element() {
     printf("TEST: single element operations ... ");
     struct List_t* list = list_create();
     int* a = malloc(sizeof(int)); *a = 100;
     list_push_front(list, 1, a);
     assert(list_head(list) == list_tail(list));
-    assert(list_head(list)->key == 1);
+    assert(list_get_key(list_head(list)) == 1);
     list_move_to_front(list, list_head(list));
-    assert(list_head(list)->key == 1);
+    assert(list_get_key(list_head(list)) == 1);
     list_pop_back(list);
     assert(list_size(list) == 0);
     assert(list_head(list) == NULL);
@@ -192,7 +185,6 @@ static void test_single_element() {
     printf("PASSED\n");
 }
 
-// Тест 8: Работа с двумя элементами
 static void test_two_elements() {
     printf("TEST: two elements operations ... ");
     struct List_t* list = list_create();
@@ -200,22 +192,23 @@ static void test_two_elements() {
     int* b = malloc(sizeof(int)); *b = 200;
     list_push_front(list, 1, a);
     list_push_front(list, 2, b);
-    assert(list_head(list)->next == list_tail(list));
-    assert(list_tail(list)->prev == list_head(list));
+    assert(list_get_key(list_head(list)) == 2);
+    assert(list_get_key(list_tail(list)) == 1);
+    assert(list_node_get_next(list_head(list)) == list_tail(list));
+    assert(list_node_get_prev(list_tail(list)) == list_head(list));
     list_move_to_front(list, list_tail(list));
-    assert(list_head(list)->key == 1);
-    assert(list_tail(list)->key == 2);
+    assert(list_get_key(list_head(list)) == 1);
+    assert(list_get_key(list_tail(list)) == 2);
     list_remove_node(list, list_head(list));
     assert(list_size(list) == 1);
-    assert(list_head(list)->key == 2);
-    assert(list_tail(list)->key == 2);
+    assert(list_get_key(list_head(list)) == 2);
+    assert(list_get_key(list_tail(list)) == 2);
     free(a);
     free(b);
     list_free(list);
     printf("PASSED\n");
 }
 
-// Тест 9: Хранение динамических значений (строки)
 static void test_dynamic_values() {
     printf("TEST: dynamic values (strings) ... ");
     struct List_t* list = list_create();
@@ -226,8 +219,8 @@ static void test_dynamic_values() {
     list_push_front(list, 2, s2);
     list_push_front(list, 3, s3);
     assert(list_size(list) == 3);
-    assert(strcmp((char*)list_head(list)->value, "!!!") == 0);
-    assert(strcmp((char*)list_tail(list)->value, "Hello") == 0);
+    assert(strcmp((char*)list_get_value(list_head(list)), "!!!") == 0);
+    assert(strcmp((char*)list_get_value(list_tail(list)), "Hello") == 0);
     free(s1);
     free(s2);
     free(s3);
